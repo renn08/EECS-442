07fd3fd3d319ae89965c005b090fbe79ffe543ed
renn08 <renn_08@outlook.com>
2021-12-22 22:52:00 -0500

    finish hw0

--- HW/hw0/visualize/mystery_visualize.py
+++ HW/hw0/visualize/mystery_visualize.py
@@ -17,13 +17,40 @@ def colormapArray(X, colors):
     Outputs:
         a HxW uint8 image using the given colormap
     """
+    N, M = np.shape(colors)
+    # print(N)
 
     return None
 
 
 if __name__ == "__main__":
     colors = np.load("mysterydata/colors.npy")
-    data = np.load("mysterydata/mysterydata.npy")
+    data = np.load("mysterydata/mysterydata3.npy")
+    row, col, hei = np.shape(data)
+    # for data2
+    # im1 = np.sum((((data / 1.3 - data.min()) / (data.max() - data.min())) * 1023).astype(np.uint8) , axis = 2)
+
+    # for data4
+    # im1 = np.sum((((data - data.min()) / (data.max() - data.min())) * 1023).astype(np.uint8), axis=2)
+
+    # for data3
+    im1 = np.sum((((data / 1.05 - np.nanmin(data)) / (np.nanmax(data) - np.nanmin(data))) * 1023).astype(np.uint8), axis=2)
+
+    red = np.zeros((row, col))
+    green = np.zeros((row, col))
+    blue = np.zeros((row, col))
+
+    for i in range(row):
+        for j in range(col):
+            for k in range(3):
+                index = im1[i][j]
+                red[i][j] = colors[index][0]
+                green[i][j] = colors[index][1]
+                blue[i][j] = colors[index][2]
+    im = np.dstack((red, green, blue))
+    imgplot = plt.imshow(im)
+    plt.imsave("mysterydata3.png", im)
+    plt.show()
 
 
-    pdb.set_trace()
+    # pdb.set_trace()
